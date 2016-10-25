3eec659a3a0efec58cb37d004241fb826543fbe5
Managam Silalahi <managam@yesbossnow.com>
2016-10-25 17:35:42 +0700

    Lots of greetings

--- Props/index.ios.js
+++ Props/index.ios.js
@@ -5,17 +5,37 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry, Image
-} from 'react-native';
+// import {AppRegistry, Image} from 'react-native';
 
-export default class Props extends Component {
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
+// export default class Props extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
+//     return (
+//       <Image source={pic} style={{width: 193, height: 110}}/>
+//     );
+//   }
+// }
+
+import {AppRegistry, Text, View} from 'react-native';
+
+class Greeting extends Component {
+  render(){
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Props extends Component {
+  render(){
+    return (
+      <View style={{alignItems: "center"}}>
+        <Greeting name = "Managam" />
+        <Greeting name = "Raja" />
+        <Greeting name = "Silalahi" />
+      </View>
     );
   }
 }
