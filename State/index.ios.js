0a0f4639de8bcf24d9267f500e583ecdffaa51ed
Managam Silalahi <managam@yesbossnow.com>
2016-10-25 17:53:59 +0700

    Add State project

---
+++ State/index.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import {AppRegistry, Text, View} from "react-native";
+
+class Blink extends Component {
+  constructor(props){
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render(){
+    let display = this.state.showText ? this.props.text : "";
+    return(
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class State extends Component {
+  render(){
+    return (
+      <View style={{alignItems: "center"}}>
+        <Blink text = "I love to blink" />
+        <Blink text = "Yes blinking is so great" />
+        <Blink text = "Why did they ever take this out of HTM" />
+        <Blink text = "Look at me look at me look at me" />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('State', () => State);
