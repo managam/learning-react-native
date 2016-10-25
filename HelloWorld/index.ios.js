ce4725524a772e2b85a6a4793c83f7ae24e3de0a
Managam Silalahi <managam@yesbossnow.com>
2016-10-25 16:44:17 +0700

    Hello world project edited

--- HelloWorld/index.ios.js
+++ HelloWorld/index.ios.js
@@ -5,14 +5,30 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-class HelloWorldApp extends Component {
+export default class HelloWorld extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
