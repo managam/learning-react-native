ca42a05354e8ae67832ee04e974f654392cc5990
Managam Silalahi <managam@yesbossnow.com>
2016-10-27 15:02:38 +0700

    Add FixedDimensions project

---
+++ FixedDimensions/index.ios.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from "react";
+import {AppRegistry, View} from "react-native";
+
+class FixedDimensions extends Component {
+  render(){
+    return(
+      <View>
+        <View style = {{width: 50, height: 50, backgroundColor: "powderblue"}} />
+        <View style = {{width: 100, height: 100, backgroundColor: "skyblue"}} />
+        <View style = {{width: 150, height: 150, backgroundColor: "steelblue"}} />
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent("FixedDimensions", () => FixedDimensions);
